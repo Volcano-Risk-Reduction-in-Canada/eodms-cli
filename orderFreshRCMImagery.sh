2fbbc743db6a9e530ad36548ef79fe01701fe0ab
Drew Rotheram <drew.rotheram-clarke@canada.ca>
2024-02-20 06:23:05 -0800

    add new sites

--- orderFreshRCMImagery.sh
+++ orderFreshRCMImagery.sh
@@ -11,7 +11,6 @@ CURRENT=`date +"%Y%m%d"  --date='+2 day'`
 PREVIOUS=`date +"%Y%m%d" --date='-1 day'`
 
 ###### A4201 Meager ######
-
 BEAMS=(5M3 5M2 5M8 5M10 5M15 5M21)
 
 for BEAM in ${BEAMS[@]};
@@ -44,7 +43,6 @@ do
 done
 
 # A4202 Garibaldi
-
 BEAMS=(3M6 3M7 3M18 3M23 3M30 3M34 3M42)
 
 for BEAM in ${BEAMS[@]};
@@ -77,7 +75,6 @@ do
 done
 
 # A4203 Cayley
-
 BEAMS=(3M1 3M6 3M13 3M14 3M17 3M24 3M30 3M36)
 
 for BEAM in ${BEAMS[@]};
@@ -110,7 +107,6 @@ do
 done
 
 # A4204 Edziza North
-
 BEAMS=(3M13 3M41)
 
 for BEAM in ${BEAMS[@]};
@@ -143,7 +139,6 @@ do
 done
 
 # A4205 Edziza South
-
 BEAMS=(3M12 3M31 3M42)
 
 for BEAM in ${BEAMS[@]};
@@ -176,7 +171,6 @@ do
 done
 
 # A4206 Tseax
-
 BEAMS=(3M7 3M9 3M19 3M40)
 
 for BEAM in ${BEAMS[@]};
@@ -209,7 +203,6 @@ do
 done
 
 # A4207 Nazko 
-
 BEAMS=(3M9 3M20 3M31)
 
 for BEAM in ${BEAMS[@]};
@@ -242,7 +235,6 @@ do
 done
 
 # A4208 Hoodoo
-
 BEAMS=(3M8 3M14 3M38 3M41 3M11)
 
 for BEAM in ${BEAMS[@]};
@@ -275,7 +267,6 @@ do
 done
 
 # A4209 Lava Fork
-
 BEAMS=(3M11 3M20 3M21 3M29 3M31 3M41)
 
 for BEAM in ${BEAMS[@]};
@@ -308,7 +299,6 @@ do
 done
 
 # Edgecumbe
-
 BEAMS=(3M36)
 
 for BEAM in ${BEAMS[@]};
@@ -334,6 +324,139 @@ do
         --beam=${BEAM}D \
         --imageDate=$date
       aws s3 mv $file s3://vrrc-rcm-raw-data-store/Lava_Fork/${BEAM}D/
+    done
+  else
+    echo "No Images Downloaded"
+  fi
+done
+
+# Campi Flegrei
+BEAMS=(5M4 5M18)
+
+for BEAM in ${BEAMS[@]};
+do
+  python3 ~/eodms-cli/eodms_cli.py \
+      -c RCMImageProducts \
+      -d ${PREVIOUS}-${CURRENT} \
+      -i ~/eodms-cli/AOIs/Volcano_Campi_Flegrei.kml \
+      -prc full \
+      -ov 10 \
+      -f "RCMImageProducts.BEAM_MNEMONIC="${BEAM} \
+      -pri high -s
+
+  if ls ~/eodms-cli/downloads/*.zip &>/dev/null
+  then
+    echo "RCM Images Downloaded"
+    for file in ~/eodms-cli/downloads/*zip
+    do
+      date=${file#*$BEAM}
+      date=${date:1:8}
+      python3 ~/eodms-cli/createImageSqs.py \
+        --site=Campi_Flegrei \
+        --beam=${BEAM} \
+        --imageDate=$date
+      aws s3 mv $file s3://vrrc-rcm-raw-data-store/Campi_Flegrei/${BEAM}/
+    done
+  else
+    echo "No Images Downloaded"
+  fi
+done
+
+# Vesuvius
+BEAMS=(5M2 5M20)
+
+for BEAM in ${BEAMS[@]};
+do
+  python3 ~/eodms-cli/eodms_cli.py \
+      -c RCMImageProducts \
+      -d ${PREVIOUS}-${CURRENT} \
+      -i ~/eodms-cli/AOIs/Volcano_Vesuvius.kml \
+      -prc full \
+      -ov 10 \
+      -f "RCMImageProducts.BEAM_MNEMONIC="${BEAM} \
+      -pri high -s
+
+  if ls ~/eodms-cli/downloads/*.zip &>/dev/null
+  then
+    echo "RCM Images Downloaded"
+    for file in ~/eodms-cli/downloads/*zip
+    do
+      date=${file#*$BEAM}
+      date=${date:1:8}
+      python3 ~/eodms-cli/createImageSqs.py \
+        --site=Vesuvius \
+        --beam=${BEAM} \
+        --imageDate=$date
+      aws s3 mv $file s3://vrrc-rcm-raw-data-store/Vesuvius/${BEAM}/
+    done
+  else
+    echo "No Images Downloaded"
+  fi
+done
+
+# Fagradalsfjall
+BEAMS=(5M13 5M9 5M17 5M18 5M22 5M4)
+
+for BEAM in ${BEAMS[@]};
+do
+  python3 ~/eodms-cli/eodms_cli.py \
+      -c RCMImageProducts \
+      -d ${PREVIOUS}-${CURRENT} \
+      -i ~/eodms-cli/AOIs/Fagradalsfjall.kml \
+      -prc full \
+      -ov 10 \
+      -f "RCMImageProducts.BEAM_MNEMONIC="${BEAM} \
+      -pri high -s
+
+  if ls ~/eodms-cli/downloads/*.zip &>/dev/null
+  then
+    echo "RCM Images Downloaded"
+    for file in ~/eodms-cli/downloads/*zip
+    do
+      date=${file#*$BEAM}
+      date=${date:1:8}
+      python3 ~/eodms-cli/create_slc_record.py \
+          --site=Fagradalsfjall \
+          --beam=$BEAM \
+          --image_file_name=$file
+
+      python3 ~/eodms-cli/createImageSqs.py \
+        --site=Fagradalsfjall \
+        --beam=${BEAM} \
+        --imageDate=$date
+      aws s3 mv $file s3://vrrc-rcm-raw-data-store/Fagradalsfjall/${BEAM}/
+    done
+  else
+    echo "No Images Downloaded"
+  fi
+done
+
+# Thompson River Valley
+BEAMS=(3M14)
+
+for BEAM in ${BEAMS[@]};
+do
+  python3 ~/eodms-cli/eodms_cli.py \
+      -c RCMImageProducts \
+      -d ${PREVIOUS}-${CURRENT} \
+      -i ~/eodms-cli/AOIs/Thompson_River_Valley.kml \
+      -prc full \
+      -ov 10 \
+      -f "RCMImageProducts.BEAM_MNEMONIC="${BEAM} \
+      -pri high -s
+
+  if ls ~/eodms-cli/downloads/*.zip &>/dev/null
+  then
+    echo "RCM Images Downloaded"
+    for file in ~/eodms-cli/downloads/*zip
+    do
+      date=${file#*$BEAM}
+      date=${date:1:8}
+      python3 ~/eodms-cli/createImageSqs.py \
+        --site=Thompson_River_Valley \
+        --beam=${BEAM} \
+        --imageDate=$date
+      aws s3 mv $file s3://vrrc-rcm-raw-data-store/Thompson_River_Valley/${BEAM}/
     done
   else
     echo "No Images Downloaded"
